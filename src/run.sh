7193e44a1b2b17a39d26051fb8e80a79514b3026
ArnaudDeza <64933703+ArnaudDeza@users.noreply.github.com>
2023-04-04 15:26:09 -0400

    compute canada fix

--- src/run.sh
+++ src/run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=mat_mult
+#SBATCH --job-name=test1
 #SBATCH --time=00:10:00
 #SBATCH --account=def-khalile2
-
+#SBATCH --mail-user=arnaud.deza@mail.utoronto.ca
+#SBATCH --mail-type=ALL
 
 module load python
 module load mycplex/20.1.0
 
 source cplex_20_Matrix_Mult/bin/activate
 
-#pip install --no-index --upgrade pip
-#pip install numpy==1.21.2+computecanada
 
-python main.py
-
+python main.py 2 2 2 7
 deactivate
