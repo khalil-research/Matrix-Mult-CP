55f3596eb9de601bdd1903d342e6662cd4900c9e
CpHaddock <pashootan@gmail.com>
2023-04-10 13:20:06 -0400

    Missed an update to run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=test1
 #SBATCH --time=00:10:00
 #SBATCH --account=def-khalile2
-#SBATCH --mail-user=arnaud.deza@mail.utoronto.ca
-#SBATCH --mail-type=ALL
+## SBATCH --mail-user=arnaud.deza@mail.utoronto.ca
+## SBATCH --mail-type=ALL
 
-module load python
-module load mycplex/20.1.0
+# module load python
+# module load mycplex/20.1.0
 
-source cplex_20_Matrix_Mult/bin/activate
+# source cplex_20_Matrix_Mult/bin/activate
 
 
-python main.py 2 2 2 7
-deactivate
+python main.py 2 2 2 7 cpo
+# deactivate
